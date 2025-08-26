72dc9ae23df68a4cdca7dfbfca3e6692c12b1baa
Abhay730961 <ak5934231@gmail.com>
2025-08-27 01:04:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,28 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // --- PASTE YOUR FIREBASE CONFIG OBJECT HERE ---
-    const firebaseConfig = {
-      apiKey: "AIza...",
-      authDomain: "your-project.firebaseapp.com",
-      // ... rest of your config properties
-    };
-    
-    // --- Initialize Firebase ---
-    firebase.initializeApp(firebaseConfig);
-    const auth = firebase.auth();
-    const db = firebase.firestore();
-    const vouchersCollection = db.collection('vouchers');
+    <script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDDkVr_8XVPQFDfo5q-8eVjuIvJHBbZ_2M",
+    authDomain: "web-voucher-app.firebaseapp.com",
+    databaseURL: "https://web-voucher-app-default-rtdb.firebaseio.com",
+    projectId: "web-voucher-app",
+    storageBucket: "web-voucher-app.firebasestorage.app",
+    messagingSenderId: "290727151353",
+    appId: "1:290727151353:web:bc8dba8830446585834428",
+    measurementId: "G-E4V61MC0NS"
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 
     // --- SELECTORS ---
     // (Selectors for forms, buttons, sections, etc.)
