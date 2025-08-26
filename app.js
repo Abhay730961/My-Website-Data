135a668421db80defed26d92e54385aee35127e8
Abhay730961 <ak5934231@gmail.com>
2025-08-27 01:43:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 // --- PASTE YOUR FIREBASE CONFIG OBJECT HERE ---
 const firebaseConfig = {
-    apiKey: "AIzaSyDDkVr_8XVPQFDfo5q-8eVjuIvJHBbZ_2M",
-    authDomain: "web-voucher-app.firebaseapp.com",
-    databaseURL: "https://web-voucher-app-default-rtdb.firebaseio.com",
-    projectId: "web-voucher-app",
-    storageBucket: "web-voucher-app.firebasestorage.app",
-    messagingSenderId: "290727151353",
-    appId: "1:290727151353:web:bc8dba8830446585834428",
-    measurementId: "G-E4V61MC0NS"
-  };
+    apiKey: "AIzaSyDDkVr_8XVPQFDfo5q-8eVjuIvJHBbZ_2M",
+    authDomain: "web-voucher-app.firebaseapp.com",
+    projectId: : "web-voucher-app",
+    storageBucket: "web-voucher-app.firebasestorage.app",
+    messagingSenderId: "290727151353",
+    appId: "1:290727151353:web:bc8dba8830446585834428"
+};
 // --- INITIALIZE FIREBASE APP ---
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
